a47ed1a63c9e1936460014f08fd0ba69ee86e419
Chris Adeyemi <x00198887@mytudublin.ie>
2025-12-03 14:01:24 +0000

    added search bar and populated page with movies

---
+++ Movieapp/Models/OmdbModels.cs
@@ -0,0 +1,20 @@
+﻿namespace Movieapp.Models
+{
+    public class OmdbSearchResponse
+    {
+        public List<MovieSummary>? Search { get; set; }
+        public string totalResults { get; set; }
+        public string Response { get; set; }
+        public string Error { get; set; }
+    }
+
+    public class MovieSummary
+    {
+        public string Title { get; set; }
+        public string Year { get; set; }
+        public string imdbID { get; set; }
+        public string Type { get; set; }
+        public string Poster { get; set; }
+        public string Response { get; set; }
+    }
+}
